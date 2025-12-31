3440c37655a65c514fc01441aaf57d70d093ef77
Rupesh Dwivedi <rkdwivedi933@gmail.com>
2025-12-31 12:37:56 +0530

    footer update

--- src/components/layout/FooterComponent.jsx
+++ src/components/layout/FooterComponent.jsx
@@ -182,7 +182,8 @@ const FooterComponent = () => {
           </div>
 
           {/* Bottom */}
-          <div className="flex flex-col items-center justify-center gap-4 text-sm text-gray-300 text-center">
+          <div className="flex flex-col md:flex-row justify-between gap-4 text-sm text-gray-300 
+                text-center md:text-left items-center md:items-start">
             <div>© {new Date().getFullYear()} <span className="text-amber-300">Harsh Enterprises</span> </div>
             <div className="flex gap-4">
               <Link to="/privacy-policy">Privacy Policy</Link>
